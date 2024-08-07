dc466c0765b2f1513f5e0f91f8d8f16419254beb
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-08-06 22:09:26 -0400

    exported function and added documentation

--- jobscraper.go
+++ jobscraper.go
@@ -1,18 +1,36 @@
-package main
+// some package description
+package JobScrapper
 
 import (
+	"errors"
 	"fmt"
-	"net/http"
+	"io"
+	"net/http" // https://pkg.go.dev/net/http
 	"os"
 )
 
-// https://pkg.go.dev/net/http
+// fetches the content of a url and returns the body as a string
+func GetUrlText(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", errors.New("Failed to fetch url")
+	}
+	
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.New("Failed to read body of fetched url")
+	}
+
+	return string(body), nil
+}
 
 func main() {
-	resp, err := http.Get("http://example.com/")
+	url := "https://example.com/"
+	resp, err := GetUrlText(url)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(resp.Status)
+	fmt.Println(resp)
 }
